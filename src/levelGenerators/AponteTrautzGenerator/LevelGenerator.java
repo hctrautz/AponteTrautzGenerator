074892c9d35bedf0d2edfbcab51bb85e9a84d9eb
Hunter Trautz <hctrautz@wpi.edu>
2019-11-01 20:11:39 -0400

    Cleaned up code and added TODOs

--- src/levelGenerators/AponteTrautzGenerator/LevelGenerator.java
+++ src/levelGenerators/AponteTrautzGenerator/LevelGenerator.java
@@ -6,12 +6,11 @@ import engine.core.MarioTimer;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Random;
 
 public class LevelGenerator implements MarioLevelGenerator{
+    int [] order = runMarkov();
     private Random rnd;
     private int sampleWidth = 30;
     private String folderName = "levels/original/";
@@ -24,24 +23,7 @@ public class LevelGenerator implements MarioLevelGenerator{
     private final float ENMEY_PROB = 0.1f;
     private final int FLOOR_PADDING = 3;
 
-    public LevelGenerator(){}
-
-//    public LevelGenerator(String sampleFolder) {
-//        this(sampleFolder, 10);
-//    }
-//
-//    public LevelGenerator(String sampleFolder, int sampleWidth) {
-//        this.sampleWidth = sampleWidth;
-//        this.folderName = sampleFolder;
-//    }
-
-    //Frequency table
-    Map<String, Integer> freq = new HashMap<String, Integer>() {
-        @Override
-        public Integer get(Object key) {
-            return containsKey(key) ? super.get(key) : 0;
-        }
-    };
+    public LevelGenerator() throws IOException {}
 
     private String getLevel(int lvl) throws IOException {
         File[] listOfFiles = new File(folderName).listFiles();
@@ -56,6 +38,7 @@ public class LevelGenerator implements MarioLevelGenerator{
     public int [] runMarkov() throws IOException{
         int [] output = new int[999];
         // the state transition matrix
+        //TODO: Expand to using all 15 original levels and actually calculate values regarding the probability
         double[][] transition = { { 0.386, 0.147, 0.202, 0.062, 0.140, 0.047, 0.016},
                 { 0.107, 0.267, 0.227, 0.120, 0.207, 0.052, 0.020},
                 { 0.035, 0.101, 0.188, 0.191, 0.357, 0.067, 0.061},
@@ -90,11 +73,11 @@ public class LevelGenerator implements MarioLevelGenerator{
     }
 
     public String getGeneratedLevel(MarioLevelModel model, MarioTimer timer) throws IOException {
-        int [] order = runMarkov();
         rnd = new Random();
         model.clearMap();
         for(int i=0; i<model.getWidth() / sampleWidth; i++){
             try {
+                //TODO: Only copy the lines of code from the level that will match up with what we already have
                 model.copyFromString(i*sampleWidth, 0, i*sampleWidth, 0, sampleWidth, model.getHeight(), getLevel(order[i]));
             } catch (IOException e) {
                 e.printStackTrace();
